040fd126e23339d0cfa5bc7c33e8d32a29d01ffe
Dharamveer Gupta <dharamveer.gupt@gmail.com>
2025-09-17 02:45:44 +0530

    feat: Initial project setup
    - Added AndroidManifest.xml and build.gradle.kts for app module.
    - Configured project with initial dependencies including Compose, Room, Hilt, Coroutines, Navigation, WorkManager and Coil.
    - Added default .gitignore, Proguard rules, and IDE specific configuration files.
    - Included launcher icons and basic theme setup.
    - Added MainActivity, SpendWiseApp, and example unit/instrumented tests.
    - Configured Gradle wrapper and project-level build files.

---
+++ app/build.gradle.kts
@@ -0,0 +1,95 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.ksp)
+    alias(libs.plugins.hilt.android)
+}
+
+android {
+    namespace = "com.dharamveer.spendwise"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.dharamveer.spendwise"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    @Suppress("UnstableApiUsage")
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.13" // 👈 This is required
+    }
+}
+
+dependencies {
+
+    // Core + Compose
+    implementation(libs.androidx.core.ktx)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.activity.compose)
+
+    // Room
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+
+    // Navigation
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    implementation(libs.workmanager)
+    implementation(libs.coil.compose)
+
+    // Debug tools
+    debugImplementation(libs.androidx.compose.ui.tooling)
+
+    // Tests
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+}
---
+++ app/src/main/java/com/dharamveer/spendwise/SpendWiseApp.kt
@@ -0,0 +1,7 @@
+package com.dharamveer.spendwise
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SpendWiseApp : Application()
